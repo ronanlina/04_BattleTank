6a4adadffab56ab34b7695debb56a822fff743f5
Ronan Lina <ronanlina@gmail.com>
2018-05-22 01:51:20 +0800

    S4:129

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Controlled Tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *ControlledTank->GetName())
+	}
+	UE_LOG(LogTemp, Warning, TEXT("AI On Begin Play"));
+
+}
